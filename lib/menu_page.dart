b78f972cc52186995c3d7dd23e0b6a5c0edf4568
dl-dl <dl.toronto@outlook,com>
2025-02-14 10:03:06 -0400

    Minor change

--- lib/menu_page.dart
+++ lib/menu_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:provider/provider.dart' show Provider;
 
 import 'about_page.dart' show AboutPage;
 import 'user_page.dart' show UserPage;
