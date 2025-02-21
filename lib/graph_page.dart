508afb49f0d02d2a77ee7bf3b4082b95d398769b
Dmitry L <dl-dl@users.noreply.github.com>
2025-02-21 13:39:42 -0400

    Tare the signal by computing an average (#23)

--- lib/graph_page.dart
+++ lib/graph_page.dart
@@ -1,10 +1,17 @@
+import 'dart:collection';
 import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:universal_ble/universal_ble.dart'
     show AvailabilityState, BleDevice, BleService;
 
 import 'bt_handling.dart' show BluetoothHandling;
+
+const int numAdcChan = 4;
+const int numGraphLines = 2;
+const int graphWindow = 200;
+const int avgWindow = 256;
 
 class GraphPage extends StatefulWidget {
   const GraphPage({super.key});
@@ -16,13 +23,16 @@ class GraphPage extends StatefulWidget {
 
 class _GraphPageState extends State<GraphPage> {
   final BluetoothHandling _bluetoothHandler = BluetoothHandling();
+  bool _graphFilerAverage = true;
 
-  final List<List<int>> decodedChannels = [];
-  final List<int> decodedStatus = [];
-  final List<int> decodedCRC = [];
+  final List<Queue<FlSpot>> chartDataCh = List.generate(
+    numGraphLines,
+    (_) => Queue<FlSpot>(),
+    growable: false,
+  );
 
-  List<FlSpot> chartDataCh1 = [FlSpot(0, 0)];
-  List<FlSpot> chartDataCh2 = [FlSpot(0, 0)];
+  final AvgAdcData avgAdcData = AvgAdcData();
+  int xVal = 0;
 
   @override
   void initState() {
@@ -34,9 +44,7 @@ class _GraphPageState extends State<GraphPage> {
 
   void processReceivedData(Uint8List data) {
     _parseAndAppendDataPacket(data);
-    setState(() {
-      _updateChartData(); // Update UI layer
-    });
+    setState(() {}); // Update UI layer
   }
 
   void _parseAndAppendDataPacket(Uint8List data) {
@@ -45,44 +53,34 @@ class _GraphPageState extends State<GraphPage> {
     }
 
     for (int packetStart = 0; packetStart < data.length; packetStart += 15) {
-      assert(packetStart + 14 < data.length);
-      final status = (data[packetStart + 1] << 8) | data[packetStart];
-      final channels = List.generate(4, (i) {
+      assert(packetStart + 15 <= data.length);
+      // final status = (data[packetStart + 1] << 8) | data[packetStart];
+      final Int32List channels = Int32List(numAdcChan);
+      for (int i = 0; i < channels.length; ++i) {
         int baseIndex = packetStart + 2 + i * 3;
-        return ((data[baseIndex + 2] << 16) |
+        channels[i] = ((data[baseIndex + 2] << 16) |
                 (data[baseIndex + 1] << 8) |
                 data[baseIndex])
             .toSigned(24);
-      });
-      final crc = data[packetStart + 14];
+      }
+      // final crc = data[packetStart + 14];
 
-      decodedChannels.add(channels);
-      decodedStatus.add(status);
-      decodedCRC.add(crc);
+      avgAdcData.add(channels);
+      for (int i = 0; i < numGraphLines; ++i) {
+        final int adcChan = i + 1;
+        chartDataCh[i].add(FlSpot(
+            xVal.toDouble(),
+            channels[adcChan].toDouble() +
+                (xVal % (7 + i * 4)) -
+                (_graphFilerAverage ? avgAdcData.getAvg(adcChan) : 0)));
+      }
+      xVal++;
+      if (chartDataCh[0].length > graphWindow) {
+        for (int i = 0; i < numGraphLines; ++i) {
+          chartDataCh[i].removeFirst();
+        }
+      }
     }
-  }
-
-  void _updateChartData() {
-    const int window = 6400;
-    final int start = (decodedChannels.length <= window)
-        ? 0
-        : decodedChannels.length - window;
-
-    chartDataCh1 = List.generate(
-      decodedChannels.length - start,
-      (i) => FlSpot(
-        (i + start).toDouble(),
-        decodedChannels[i + start][2].toDouble(),
-      ),
-    );
-
-    chartDataCh2 = List.generate(
-      decodedChannels.length - start,
-      (i) => FlSpot(
-        (i + start).toDouble(),
-        decodedChannels[i + start][1].toDouble(),
-      ),
-    );
   }
 
   @override
@@ -100,7 +98,7 @@ class _GraphPageState extends State<GraphPage> {
           BluetoothIndicator(bluetoothService: _bluetoothHandler),
           IconButton(
             icon: const Icon(Icons.refresh),
-            tooltip: 'Refresh BT Icon',
+            tooltip: 'Refresh BT',
             onPressed: _bluetoothHandler.toggleScan,
           ),
         ],
@@ -109,26 +107,40 @@ class _GraphPageState extends State<GraphPage> {
         children: [
           Expanded(
               child: BluetoothDeviceList(bluetoothService: _bluetoothHandler)),
+          Checkbox(
+            value: _graphFilerAverage,
+            onChanged: (bool? newValue) {
+              _graphFilerAverage = newValue!;
+              setState(() {});
+            },
+          ),
           Expanded(
-              child: LineChart(
-            LineChartData(
-              lineBarsData: ([
-                LineChartBarData(
-                    spots: chartDataCh1,
-                    dotData: FlDotData(
+            child: LineChart(
+              LineChartData(
+                lineBarsData: ([
+                  LineChartBarData(
+                    spots: chartDataCh[0].toList(growable: false),
+                    dotData: const FlDotData(
                       show: false,
-                    )),
-                LineChartBarData(
-                    spots: chartDataCh2,
-                    dotData: FlDotData(
+                    ),
+                    color: Colors.blueAccent,
+                  ),
+                  LineChartBarData(
+                    spots: chartDataCh[1].toList(growable: false),
+                    dotData: const FlDotData(
                       show: false,
-                    ))
-              ]),
-              minY:
-                  0, // TODO make this into min(data, 0), as negative values go outside the chart
+                    ),
+                    color: Colors.deepOrangeAccent,
+                  )
+                ]),
+                minY: 0,
+                clipData: const FlClipData.all(),
+              ),
+              duration: Duration.zero,
+              //duration: const Duration(milliseconds: 1000),
+              curve: Curves.linear,
             ),
-            //duration: const Duration(milliseconds: 1000),
-          )),
+          ),
         ],
       ),
       floatingActionButton: ValueListenableBuilder<bool>(
@@ -142,6 +154,30 @@ class _GraphPageState extends State<GraphPage> {
         },
       ),
     );
+  }
+}
+
+class AvgAdcData {
+  final Float64List avg = Float64List(numAdcChan);
+  final Int64List runningTotal = Int64List(numAdcChan);
+  int count = avgWindow;
+
+  void add(Int32List val) {
+    for (int i = 0; i < numAdcChan; ++i) {
+      runningTotal[i] += val[i];
+    }
+    count--;
+    if (count == 0) {
+      count = avgWindow;
+      for (int i = 0; i < runningTotal.length; ++i) {
+        avg[i] = runningTotal[i].toDouble() / avgWindow;
+        runningTotal[i] = 0;
+      }
+    }
+  }
+
+  double getAvg(int idx) {
+    return avg[idx];
   }
 }
 
