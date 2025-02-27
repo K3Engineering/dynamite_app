84e269eab8f3a12c8e39e9a608b369632dda6fe2
dl-dl <dl.toronto@outlook,com>
2025-02-27 11:47:37 -0400

    Cosmetic

--- lib/mockble.dart
+++ lib/mockble.dart
@@ -9,8 +9,6 @@ class MockBlePlatform extends UniversalBlePlatform {
   static MockBlePlatform get instance => _instance ??= MockBlePlatform._();
   static const netDelay = Duration(seconds: 1);
   static const hwDelay = Duration(milliseconds: 200);
-  static const int _samplesInPack = 16;
-  static const dataInterval = Duration(milliseconds: 1 * _samplesInPack);
 
   MockBlePlatform._() {
     _setupListeners();
@@ -158,10 +156,12 @@ class MockBlePlatform extends UniversalBlePlatform {
     _notificationTimer?.cancel();
     _notificationTimer = null;
     if (BleInputProperty.notification == bleInputProperty) {
+      const int samplesPerPack = 16;
+      const dataInterval = Duration(milliseconds: 1 * samplesPerPack);
       final Uint8List sample =
           Uint8List.fromList([0, 0, 5, 4, 3, 6, 5, 4, 7, 6, 5, 8, 7, 6, 0]);
-      final Uint8List ev = Uint8List(sample.length * _samplesInPack);
-      for (int i = 0; i < _samplesInPack; ++i) {
+      final Uint8List ev = Uint8List(sample.length * samplesPerPack);
+      for (int i = 0; i < samplesPerPack; ++i) {
         for (int j = 0; j < sample.length; ++j) {
           ev[i * sample.length + j] = sample[j];
         }
