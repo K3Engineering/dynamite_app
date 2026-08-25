8434a161416ca4b3f4b785159df0152bf9eeea8f
Vlad K <vlad@k3engineering.com>
2026-08-25 10:29:14 -0400

    adjust some tests to use the new flutter material include

--- test/live_status_bar_rssi_test.dart
+++ test/live_status_bar_rssi_test.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:flutter/material.dart';
+import 'package:material_ui/material_ui.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
 import 'package:universal_ble/universal_ble.dart';
